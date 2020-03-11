016c23bf2ac5921df45d8c34407eebc4d084eaaa
obi3965 <5571142@RTS-365.dk>
2020-03-11 21:21:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ var app = express();
 var router = require("./router");
 var { testConnection } = require("./config/database");
 var formidable = require("express-formidable");
-<<<<<<< HEAD
 var cors = require('cors')
-=======
 var cors = require("cors");
 
->>>>>>> f5ee4b2c0a023dc50cd861e280b3e1e05b8fbe26
 testConnection();
 app.use(cors());
 app.use(formidable());
